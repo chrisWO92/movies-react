1eb4f7a1f7a0d0b72dca3fca3cced41f91f31ec8
chrisWO92 <chrisworkingonline@gmail.com>
2022-07-10 20:23:32 -0300

    keep going

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,85 @@
-import React from 'react';
+import React from "react";
+import { MovieDetails } from "./pages/MovieDetails"
 import { MoviesGrid } from "./components/MoviesGrid";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 
-import {
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 /* export function App() {
-    return (
-        <div>
-            <header>
-                <h1 className={styles.title}>Movies</h1>
-            </header>
-            <main>
-                <MoviesGrid />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <h1 className={styles.title}>Movies</h1>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/movie">Movie</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/movie">Movie</Route>
+          <Route path="/">Home</Route>
+        </Routes>
+        <MoviesGrid />
+      </main>
+      </div>
+    
+  );
 } */
 
 export function App() {
-    return (
-      <div className="App">
-        <h1 className={styles.title}>Welcome to React Router!</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-        <MoviesGrid />
-      </div>
-    );
+  return (
+    <div className="App">
+      <header>
+      <h1 className={styles.title}>Welcome to React Router!</h1>
+        <Link to="/movies">Movies</Link>
+        <br />
+        <Link to="/">Home</Link>
+      </header>
+      
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/movies" element={<MovieDetails />} />
+        <Route path="/404" element={<Error />} />
+      </Routes>
+    </div>
+  );
 }
 
 function Home() {
-    return (
-      <>
-        <main>
-          <h2>Welcome to the homepage!</h2>
-          <p>You can do this, I believe in you.</p>
-        </main>
-        <nav>
-          <Link to="/about">Home</Link>
-        </nav>
-      </>
-    );
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      {/* <nav>
+        <Link to="/movies">Movies</Link>
+      </nav> */}
+    </>
+  );
 }
 
-function About() {
-    return (
-      <>
-        <main>
-          <h2>Who are we?</h2>
-          <p>
-            That feels like an existential question, don't you
-            think?
-          </p>
-        </main>
-        <nav>
-          <Link to="/">About</Link>
-        </nav>
-      </>
-    );
+function Movies() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      {/* <nav>
+        <Link to="/">Home</Link>
+      </nav> */}
+      <MoviesGrid />
+    </>
+  );
 }
+
+function Error() {
+  return (
+    <>
+      <h1>404</h1>
+    </>
+  )
+}
+
+
