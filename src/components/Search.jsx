d601780411f61589273a7a470c4f390e91089f41
chrisWO92 <chrisworkingonline@gmail.com>
2022-09-05 16:22:28 -0300

    debounce

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import styles from "./Search.module.css";
 import { FaSearch } from "react-icons/fa";
-import { useEffect,  useState } from "react";
+/* import { useEffect,  useState } from "react"; */
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "../hooks/useQuery";
 
 export function Search() {
-  const [searchText, setSearchText] = useState("");
+  /*const [searchText, setSearchText] = useState(""); Se elimina esta instrucción pues el Search pasa a controlarse a través del onChange del input del search.
 
   /* Se pone el useQuery en el Search para crear un efecto que vigile los cambios en la variable search. Si hay un cambio en la variable search, se dará la instrucción mediante un efecto para que se actualice el valor del input para que quede vacío */
   const query = useQuery();
   const search = query.get("search");
 
-  useEffect(() => {
-      /* Si se pone sólo search en el paréntesis, al actualizar la ruta, el valor del input sigue siendo el valor de la búsqueda. En cambio, si se pone search || "" quiere decir que si el valor de search no existe, se debe setear la variable searchText en comilla vacía y con esto el input se estaría actualizando correctamente a vacío cuando no se estén haciendo una búsqueda */
+  /* useEffect(() => {
+      /* Si se pone sólo search en el paréntesis, al actualizar la ruta, el valor del input sigue siendo el valor de la búsqueda. En cambio, si se pone search || "" quiere decir que si el valor de search no existe, se debe setear la variable searchText en comilla vacía y con esto el input se estaría actualizando correctamente a vacío cuando no se estén haciendo una búsqueda
       setSearchText(search || "");
-  }, [search]);
+  }, [search]); 
+  
+  El efecto anterior se elimina porque el cambio del search ahora pasa a manejarse a través del onChange del input
+
+  */
 
   /* El hook useNavigate se usa para actualizar el path en el que queremos ubicarnos. En el caso de abajo, se usa para que al hacer el submit del form, la ruta del navegador se actualice a una ruta configurada para posteriormente hacer una búsqueda en la API, que sería "/?search=" + searchText */
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    /*
     navigate("/?search=" + searchText);
+    Se elimina porque esto pasa a actualizarse en el onChange del input
+    */
   };
 
   return (
@@ -32,8 +39,15 @@ export function Search() {
         <input
           className={styles.searchInput}
           type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
+          value={search}
+          onChange={(e) => {
+            const value = e.target.value;
+            /*
+            setSearchText(value);
+            /* Se agrega la siguiente instrucción para que ante un cambio en el search, automáticamente la página empiece a cargar los elementos de la búsqueda 
+            Se elimmina elñ setSearchText pues ahora esto se controla a través del value={search} */
+            navigate("/?search=" + value)
+          }}
         />
         {/* El evento click no se pone directamente dentro del button porque este se encuentra dentro de un form.
                     Cuando un button se encuentra dentro de un form, el envío de la información del form mediante el button se
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MoviesGrid } from "../components/MoviesGrid";
 import { Search } from "../components/Search";
+import { useDebounce } from "../hooks/useDebounce";
 import { useQuery } from "../hooks/useQuery";
 
 export function LandingPage() {
@@ -12,11 +13,15 @@ export function LandingPage() {
     const query = useQuery();
     const search = query.get("search");
 
+    /* Se usa el hook useDebounce para actualizar la grilla al cambiar el texto de búsqueda sin necesidad de presionar enter ni el ícono de la lupa. Se actualiza al pasar el tiempo especificado como parámetro, en este caso 300 milisegundos. Luego, a la key del MoviesGrid se le pasa el debouncedSearch en lugar del search sólo */
+
+    const debouncedSearch = useDebounce(search, 300);
+
     return (
         <div>
             {/* Se carga la grilla y también una barra de búsqueda */}
             <Search />
-            <MoviesGrid key={search} search={search}/>
+            <MoviesGrid key={debouncedSearch} search={search}/>
         </div>
     );    
 }
